32c3a5894fd78bcb19272b74a835fa7a15950411
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-10-30 16:55:55 -0300

    Conceitos ADO.NET, SQLCommand e SQLDataReader

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+
+
+
+const string connectionString = "Server=DESKTOP-BVKU5HC;Database=balta;Trusted_Connection=True;TrustServerCertificate=True";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+
+    using (var command = new SqlCommand())
+    {
+        command.Connection = connection;
+        command.CommandType = System.Data.CommandType.Text;
+        command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+        var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+        }
+    }
+}
+
+
+
