4cda6205dbf11d2ee36d17d6d0b5d573d62dd17c
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-10-31 12:18:12 -0300

    INSERT INTO Category and Connection.Execute

--- Models/Category.cs
+++ Models/Category.cs
@@ -10,6 +10,32 @@ namespace BaltaDataAccess.Models
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
+        public string Url { get; set; }
+        public string Summary { get; set; }
+        public int Order { get; set; }
+        public string Description { get; set; }
+        public bool Featured { get; set; }
+
+        public Category(string title, string url, string summary, int order, string description, bool featured)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Url = url;
+            Summary = summary;
+            Order = order;
+            Description = description;
+            Featured = featured;
+        }
+
+        public Category(Guid id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+
+
+
 
 
     }
--- Program.cs
+++ Program.cs
@@ -2,22 +2,45 @@
 using Dapper;
 using Microsoft.Data.SqlClient;
 
-
-
 const string connectionString = "Server=DESKTOP-BVKU5HC;Database=balta;Trusted_Connection=True;TrustServerCertificate=True";
+
+
+Category category = new Category("Amazon AWS", "Amazon", "Categoria destinada a serviços AWS", 8, "AWS Cloud", false);
+
+var insertSql = @"INSERT INTO 
+    [Category] 
+        VALUES(
+            @Id, 
+            @Title, 
+            @Url, 
+            @Summary, 
+            @Order, 
+            @Description, 
+            @Featured)";
+
+
+
 
 using (var connection = new SqlConnection(connectionString))
 {
-    connection.Open();
-
-    using (var command = new SqlCommand())
+    var rows = connection.Execute(insertSql, new
     {
-        var categories = connection.Query<Category>("SELECT [ID], [Title] FROM [Category]");
-        foreach (var category in categories)
-        {
-            Console.WriteLine(category.Id + ", " + category.Title);
-        }
+        category.Id,
+        category.Title,
+        category.Url,
+        category.Summary,
+        category.Order,
+        category.Description,
+        category.Featured
+    });
+    Console.WriteLine($"{rows} linhas inseridas;");
+
+    var categories = connection.Query<Category>("SELECT [Id], [Title] Title FROM [Category]");
+    foreach (var item in categories)
+    {
+       Console.WriteLine(item.Id + ", " + item.Title);
     }
+    
 }
 
 
