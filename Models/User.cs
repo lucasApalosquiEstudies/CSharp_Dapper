000e337c7e014a73980c4cea940756f87d414abd
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-01 16:15:23 -0300

    Dapper Contrib

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Dapper.Contrib.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Blog.Models { 
+namespace Blog.Models {
+
+    [Table("[User]")]
     public class User
     {
         public int Id { get; set; } 
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+const string CONNECTION_STRING = "Server=DESKTOP-BVKU5HC;Database=Blog;Trusted_Connection=True;TrustServerCertificate=True";
+
+ReadUsers();
+
+static void ReadUsers()
+{
+    using (var connection = new SqlConnection(CONNECTION_STRING))
+    {
+        var users = connection.GetAll<User>();
+        foreach (var user in users)
+        {
+            Console.WriteLine(user.Name);
+        }
+    }
+}
