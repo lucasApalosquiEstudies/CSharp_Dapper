e90ff972481405b2f2cbfcef5f4e0114c66ddac6
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-02 12:16:40 -0300

    :sparkles: feat(Blog): add Create and GetById User Methods

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -12,13 +12,19 @@ namespace Blog.Repositories
     public class UserRepository
     {
         private readonly SqlConnection _connection;
-        
+
         public UserRepository(SqlConnection connection)
             => _connection = connection;
-        
+
 
         public IEnumerable<User> GetAll()
             => _connection.GetAll<User>();
+
+        public User GetById(int id)
+            => _connection.Get<User>(id);
+
+        public void Create(User user)
+            => _connection.Insert(user);
 
     }
 }
