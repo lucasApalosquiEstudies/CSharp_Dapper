8458cdfbabc7a8e58a25be36f8dc529f43df64b3
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-06 18:02:04 -0300

    :sparkles: feat(Blog): Add Update and Delete Role Methods

--- Repositories/RoleRepository.cs
+++ Repositories/RoleRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,36 @@ namespace Blog.Repositories
 {
     public class RoleRepository
     {
-         
+        private readonly SqlConnection _connection;
+
+        public RoleRepository(SqlConnection conenction)
+            => _connection = conenction;
+
+        public IEnumerable<Role> GetAll()
+            => _connection.GetAll<Role>();
+
+        public Role GetById(int id)
+            => _connection.Get<Role>(id);
+
+        public void Create(Role role)
+        {
+            role.Id = 0;
+            _connection.Insert(role);
+        }
+
+        public void Update(Role role)
+        {
+            if (role.Id != 0)
+            {
+                _connection.Update(role);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            Role role = GetById(id);
+            _connection.Delete(role);
+        }
+
     }
 }
