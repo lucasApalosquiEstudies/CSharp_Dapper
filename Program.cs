ef623a41ce4c5cd5b4e6a4a568870eaadaa7453a
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-07 11:52:12 -0300

    :sparkles: feat(Blog): Add GetUsersWithRoles

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ using Microsoft.Data.SqlClient;
 
 const string CONNECTION_STRING = "Server=DESKTOP-BVKU5HC;Database=Blog;Trusted_Connection=True;TrustServerCertificate=True";
 var connection = new SqlConnection(CONNECTION_STRING);
+connection.Open();
 
+connection.Close();
 
 
 
